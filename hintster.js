3c1af2c186dc99f82799946f3a62c0b5a4fe5168
unknown <djones6@EIS-TECH-32BNF6.ebsco.com>
2015-04-16 13:29:11 -0500

    Added template, reporter code.

--- hintster.js
+++ hintster.js
@@ -1,5 +1,9 @@
+var jade = require('jade'),
+    fs = require('fs');
+
 module.exports = {
     reporter: function (errors) {
-        
+        var compile = jade.compileFile('./templates/main.template.jade');
+        fs.writeFileSync('output.html', compile(errors));
     }
 };
